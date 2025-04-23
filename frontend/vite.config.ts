dbaaa7bfabf3b54d657fc17b5d962e58d2bfd259
Dev-Dam <tandam9902@gmail.com>
2025-04-23 23:49:20 +0700

    Setup env for user

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,27 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
+import { defineConfig, loadEnv, ConfigEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+
+  return {
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss()],
+      },
     },
-  },
-  server: {
-    watch: {
-      usePolling: true,
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: Number(env.VITE_PORT) || 5001,
     },
-    host: true, // Here
-    strictPort: true,
-    port: 3000,
-  },
-});
+    define: {
+      __APP_API_URL__: JSON.stringify(env.VITE_API_URL),
+    },
+  }
+})
