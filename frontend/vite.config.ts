85b0f7370aeae1284d320dff341e930cfba58ab4
Dev-Dam <tandam9902@gmail.com>
2025-04-25 08:25:43 +0700

    Fixed build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,24 @@
-import { defineConfig, loadEnv, ConfigEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 
-export default defineConfig(({ mode }: ConfigEnv) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_')
-
-  return {
-    plugins: [react()],
-    css: {
-      postcss: {
-        plugins: [tailwindcss()],
-      },
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
     },
-    server: {
-      watch: {
-        usePolling: true,
-      },
-      host: "0.0.0.0",
-      strictPort: true,
-      allowedHosts: true,
-      port: Number(env.VITE_PORT) || 5001,
+  },
+  server: {
+    watch: {
+      usePolling: true,
     },
-    define: {
-      __APP_API_URL__: JSON.stringify(env.VITE_API_URL),
-    },
-  }
+    host: "0.0.0.0",
+    strictPort: true,
+    allowedHosts: true,
+    port: 5001,
+  },
+  define: {
+    __APP_API_URL__: "http://localhost:5000",
+  },
 })
